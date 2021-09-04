ec8a47e84772f4fad183aaaae8184fa8de4d1b8a
dbaonam99 <dbaonam99@gmail.com>
2021-09-04 17:00:57 +0700

    Project initial

---
+++ src/mockData/users.js
@@ -0,0 +1,27 @@
+export const USER_OPTIONS = [
+  {
+    id: 1,
+    name: "Dương Bảo Nam",
+    age: 22,
+  },
+  {
+    id: 2,
+    name: "Dương Bảo Nam",
+    age: 22,
+  },
+  {
+    id: 3,
+    name: "Dương Bảo Nam",
+    age: 22,
+  },
+  {
+    id: 4,
+    name: "Dương Bảo Nam",
+    age: 22,
+  },
+  {
+    id: 5,
+    name: "Dương Bảo Nam",
+    age: 22,
+  }
+]
